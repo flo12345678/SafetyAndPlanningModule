76cbe6e0b8a9c53ce7d52b9aa17ec21c542f7b58
flo12345678 <f.griese@uke.de>
2016-10-27 16:35:48 +0200

    add example and test, ad convert2unit function

--- src/convert2unit.jl
+++ src/convert2unit.jl
@@ -1,6 +1,13 @@
 @doc "convert2unit(data_array,unit) converts the data array (tuples) without units to
       an array with length units."->
-function convert2unit(data_array,unit::Unitful.Length)
+function convert2unit(data_array, unit::Unitful.Length)
+         x_coord=[x[1] for x in data_array]*unit;
+         y_coord=[x[2] for x in data_array]*unit;
+         z_coord=[x[3] for x in data_array]*unit;
+         coord_array=hcat(x_coord,y_coord,z_coord);
+end
+
+function convert2unit{U}(data_array, unit::Unitful.Units{U ,Unitful.Dimensions{(Unitful.Dimension{:Length}(1//1),)}})
          x_coord=[x[1] for x in data_array]*unit;
          y_coord=[x[2] for x in data_array]*unit;
          z_coord=[x[3] for x in data_array]*unit;
